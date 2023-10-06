93045f14c0a24a3b4101e3715977869bc1f06b82
suvicsabika <81648169+suvicsabika@users.noreply.github.com>
2023-10-06 19:53:34 +0200

    Update Main.java

--- fifth_week/parancssoriArgumentumok/Main.java
+++ fifth_week/parancssoriArgumentumok/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         if (args.length != 2) {
-            System.out.println("Hiba! Adjon meg 2 szamot.");
+            System.out.println("Hiba! Pontosan ketto darab parancssori argumentumot adjon meg.");
             System.exit(1); // echo $?
         }
 
